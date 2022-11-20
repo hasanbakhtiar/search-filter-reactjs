55d80e560b556486125da5f4ca8da65cbf8d91b5
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-20 23:03:15 +0400

    search react v0.1.0

---
+++ src/components/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Users from "../data/Users";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+
+  
+  return (
+    <div className="container mt-5 me-5">
+      <div className="col-6">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search..."
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="col-6">
+        <ul className="list-group">
+          {Users.filter(u=>u.title.toLocaleLowerCase().includes(query)).map((fd, i) => (
+            <li key={i} className="list-group-item">
+              {fd.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
